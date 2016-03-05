eefa23bc8022ad18dceacd4ef2629a89db334a91
Kevin Grandon <kevingrandon@yahoo.com>
2016-03-04 22:44:37 -0800

    46, hook up buttons to LevelManager functions.

---
+++ NumberWizardUI/Assets/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel (string name) {
+		Debug.Log ("LevelLoad called for " + name);
+	}
+
+	public void QuitRequest () {
+		Debug.Log ("QuitRequest called");
+	}
+
+}
