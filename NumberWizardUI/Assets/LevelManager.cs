1d19353929879870edfb4311d6a8e5a80c29e705
Kevin Grandon <kevingrandon@yahoo.com>
2016-03-04 22:58:52 -0800

    47, add scene navigation.

--- NumberWizardUI/Assets/LevelManager.cs
+++ NumberWizardUI/Assets/LevelManager.cs
@@ -1,14 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel (string name) {
 		Debug.Log ("LevelLoad called for " + name);
+		SceneManager.LoadScene (name);
 	}
 
 	public void QuitRequest () {
 		Debug.Log ("QuitRequest called");
+		Application.Quit ();
 	}
 
 }
