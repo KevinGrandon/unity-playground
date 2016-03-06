1ad42be94305fd553da41a4479fc740956f8b689
Kevin Grandon <kevingrandon@yahoo.com>
2016-03-06 09:19:15 -0800

    74, hold ball until game starts.

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public Paddle paddle;
+	private bool started = false;
+	private Vector3 paddleToBallVector;
+	private Rigidbody2D body;
+
+	// Use this for initialization
+	void Start () {
+		body = this.GetComponent<Rigidbody2D> ();
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+		print (paddleToBallVector);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Lock the ball to the paddle at start.
+		if (!started) {
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+		}
+
+		// Wait for  amouse press to launch.
+		if (Input.GetMouseButtonDown(0)) {
+			print ("launch ball");
+			started = true;
+			body.velocity = new Vector2 (Random.Range(-2f, 2f), 10f);
+		}
+	}
+}
