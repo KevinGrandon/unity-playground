54a0b63e3999299113a45f4cc116baf645a96094
Kevin Grandon <kevingrandon@yahoo.com>
2016-03-05 18:41:20 -0800

    65, move start self-destruction into awake().

--- Block Breaker/Assets/Music.cs
+++ Block Breaker/Assets/Music.cs
@@ -3,11 +3,20 @@ using System.Collections;
 
 public class Music : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		GameObject.DontDestroyOnLoad (gameObject);
+	static Music instance = null;
+
+	void Awake () {
+		if (instance != null) {
+			Destroy (gameObject);
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);	
+		}
 	}
-	
+
+	void Start () {
+	}
+
 	// Update is called once per frame
 	void Update () {
 	
