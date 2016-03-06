fdd2c9a88be8cb1c3cc8b3ac70b229dbb50ce4e5
Kevin Grandon <kevingrandon@yahoo.com>
2016-03-06 09:02:32 -0800

    73, move paddle and clamp left and right bounds.

---
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
+		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+		mousePosInBlocks = Mathf.Clamp (mousePosInBlocks, 0.5f, 15.5f);
+		paddlePos.x = mousePosInBlocks;
+		this.transform.position = paddlePos;
+	}
+}
