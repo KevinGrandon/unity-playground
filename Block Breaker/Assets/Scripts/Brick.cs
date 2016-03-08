6e06b0bb238922c00bccb8b2a60705ce2e3099f4
Kevin Grandon <kevingrandon@yahoo.com>
2016-03-07 22:52:11 -0800

    83, swap sprites when bricks get damaged.

--- Block Breaker/Assets/Scripts/Brick.cs
+++ Block Breaker/Assets/Scripts/Brick.cs
@@ -4,14 +4,24 @@ using System.Collections;
 public class Brick : MonoBehaviour {
 
 	public int maxHits;
+	public Sprite[] hitSprites;
+
 	private int timesHit;
 	private LevelManager levelMangager;
 
 	void OnCollisionEnter2D(Collision2D collision) {
 		timesHit++;
+
 		if (timesHit >= maxHits) {
 			Destroy (gameObject, 0.1f);
+		} else {
+			LoadSprites ();
 		}
+	}
+
+	void LoadSprites () {
+		int spriteIndex = timesHit - 1;
+		this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
 	}
 
 	// Use this for initialization
